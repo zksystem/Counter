934f294e4e171bce71f2e8f26576059d7c689808
zksystem <zksystem@yahoo.com>
2022-10-11 13:49:47 +0300

    Added button control, Label outlets, Added constraints to UI elements. Added splash screen.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counterValue: Int = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBAction func counterButtonDidTouchDown(_ sender: Any) {
+        counterValue += 1
+        updateCounter()
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateCounter()
     }
-
+        
+    func updateCounter() {
+        counterLabel.text = String(counterValue)
+    }
 
 }
 
